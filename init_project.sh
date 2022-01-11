762c5efc4e1c830b70c09b518c69e0f602b23e3a
belbachir <belbachirabderrahman@gmail.com>
2022-01-11 07:26:33 +0100

    add docker based init project script + reademe

---
+++ init_project.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+printf "composer install ... \n" ;
+composer install
+
+printf "Rename .env.example to .env for eskimi project ...\n"
+cp .env.docker.example .env
+
+
+printf "Cloning laradock ...\n"
+git clone https://github.com/Laradock/laradock.git
+
+printf "go to laradock ...\n"
+cd laradock
+
+printf "Rename .env.example to .env for laradock ...\n"
+cp .env.example .env
+
+printf "launch docker-composer build ... \n" ;
+docker-compose build nginx mysql phpmyadmin redis workspace
+
+printf "launch docker-composer up -d ... \n" ;
+docker-compose up -d nginx mysql phpmyadmin redis workspace
+
+printf "docker-compose exec workspace and run artisan migrate ... \n" ;
+docker-compose exec workspace php artisan migrate
+
+printf "php artisan seed ... \n" ;
+docker-compose exec workspace php artisan db:seed --class=CompaignSeeder
+
+printf "npm install ... \n" ;
+docker-compose exec workspace npm install
+
+printf "npm run dev  ... \n" ;
+docker-compose exec workspace npm run dev 
+
